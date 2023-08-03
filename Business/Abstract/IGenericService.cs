e6d846e398d0d13c2b8f01385a411a1a488fc80b
Cugurel <cugurel7@gmail.com>
2023-08-03 16:12:40 +0300

    Services implemented

---
+++ Business/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IGenericService<Entity> where Entity : class,IEntity,new()
+    {
+        void Save(Entity entity);
+        void Delete(Entity entity);
+        void Update(Entity entity);
+        Entity GetById(int id);
+        List<Entity> GetAll();
+    }
+}
---
+++ Business/Concrete/JobManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class JobManager : IGenericService<Job>
+    {
+        public void Delete(Job entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Job> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Job GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Job entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Job entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
